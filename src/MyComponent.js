4406131086cd77f2d00c5a24928161ca5958df1e
abhinav <abhinavgr007@hotmail.com>
2019-12-30 11:32:36 +0530

    my commit

---
+++ src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div className="MyComponent">
+                this is amazing shit
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
